da35e94dfcd112c90a9af36272738cbb75f7a003
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-14 17:58:25 +0700

    Update Stats.js

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -21,48 +21,6 @@ class Stats extends React.Component {
   onChange(state) {
     this.setState(state);
   }
-
-  render() {
-    return (
-      <div className='container'>
-        <div className='panel panel-default'>
-          <table className='table table-striped'>
-            <thead>
-            <tr>
-              <th colSpan='2'>Stats</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>Total votes cast</td>
-              <td>{this.state.totalVotes}</td>
-            </tr>
-            <tr>
-              <td>Offense characters</td>
-              <td>{this.state.OffenseCount}</td>
-            </tr>
-            <tr>
-              <td>Defense characters</td>
-              <td>{this.state.DefenseCount}</td>
-            </tr>
-            <tr>
-              <td>Tank characters</td>
-              <td>{this.state.TankCount}</td>
-            </tr>
-            <tr>
-              <td>Support characters</td>
-              <td>{this.state.SupportCount}</td>
-            </tr>
-            <tr>
-              <td>Total number of characters</td>
-              <td>{this.state.totalCount}</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default Stats;
