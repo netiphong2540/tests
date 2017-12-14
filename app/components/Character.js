4723324c5106e1786d4909b43287c6bb32c2dff9
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-14 15:53:00 +0700

    Update Character.js

--- app/components/Character.js
+++ app/components/Character.js
@@ -44,13 +44,13 @@ class Character extends React.Component {
     return (
       <div className='container'>
         <div className='profile-img'>
-          <a ref='magnificPopup' className='magnific-popup' href={'https://d1u1mce87gyfbn.cloudfront.net/hero/' + this.state.name + '/hero-select-portrait.png'}>
-            <img src={'https://d1u1mce87gyfbn.cloudfront.net/hero/' + this.state.name + '/hero-select-portrait.png'} />
+          <a ref='magnificPopup' className='magnific-popup' href={'https://d1u1mce87gyfbn.cloudfront.net/hero/' + character.name + '/hero-select-portrait.png'}>
+            <img src={'https://d1u1mce87gyfbn.cloudfront.net/hero/' + character.name + '/hero-select-portrait.png'} />
           </a>
         </div>
         <div className='profile-info clearfix'>
           <h2><strong>{this.state.name}</strong></h2>
-          <h4 className='lead'>type: <strong>{this.state.type}</strong></h4>
+          <h4 className='lead'>Type: <strong>{this.state.type}</strong></h4>
           <button className='btn btn-transparent'
                   onClick={CharacterActions.report.bind(this, this.state.characterId)}
                   disabled={this.state.isReported}>
