8de453007140652bbf59a947212166bbc35f8986
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-14 15:39:05 +0700

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -27,8 +27,8 @@ class Footer extends React.Component {
     let leaderboardCharacters = this.state.characters.map(function(character) {
       return (
         <li key={character.characterId}>
-          <Link to={'/characters/' + character.characterId}>
-            <img className='thumb-md' src={'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg'} />
+          <Link to={'/characters/' + character.name}>
+            <img className='thumb-md' src={'https://d1u1mce87gyfbn.cloudfront.net/hero/' + character.name + '/hero-select-portrait.png'} />
           </Link>
         </li>
       );
@@ -38,12 +38,6 @@ class Footer extends React.Component {
       <footer>
         <div className='container'>
           <div className='row'>
-            <div className='col-sm-5'>
-              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
-              <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
-              <p>You may view the <a href='https://github.com/sahat/newedenfaces-react'>Source Code</a> behind this project on GitHub.</p>
-              <p>© 2015 Sahat Yalkabov.</p>
-            </div>
             <div className='col-sm-7 hidden-xs'>
               <h3 className='lead'><strong>Leaderboard</strong> Top 5 Characters</h3>
               <ul className='list-inline'>
