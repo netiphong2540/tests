144ef8df028e54489a15391b8e7dd648db2728e9
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-14 18:12:19 +0700

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -14,9 +14,6 @@ export default (
     <Route path='/characters/:id' component={Character} />
     <Route path='/add' component={AddCharacter} />
     <Route path=':category' component={CharacterList}>
-      <Route path=':race' component={CharacterList}>
-        <Route path=':bloodline' component={CharacterList} />
-      </Route>
     </Route>
   </Route>
 );
