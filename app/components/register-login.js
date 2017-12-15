9b71de498bef279143f187c719abb25c26c3d145
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-15 19:42:28 +0700

    Create register-login.js

---
+++ app/components/register-login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, createNetworkInterface } from 'react-apollo';
+
+import {Link} from 'react-router';
+import {isEqual} from 'underscore';
+import CharacterListStore from '../stores/CharacterListStore';
+import CharacterListActions from '../actions/CharacterListActions';
+
+import 'antd/dist/antd.css';
+
+import Routes from './routes';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:3000/graphql',
+});
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+    req.options.headers['x-token'] = localStorage.getItem('token');
+    req.options.headers['x-refresh-token'] = localStorage.getItem('refreshToken');
+    next();
+  }
+}]);
+
+const client = new ApolloClient({
+  networkInterface: networkInterface
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
