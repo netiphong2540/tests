e48b536678464576568f24be51abe37b7d130760
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-15 20:55:54 +0700

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Stats from './components/Stats';
-import register from './components/register';
 import Character from './components/Character';
 import CharacterList from './components/CharacterList';
 import AddCharacter from './components/AddCharacter';
@@ -12,7 +11,6 @@ export default (
   <Route component={App}>
     <Route path='/' component={Home} />
     <Route path='/stats' component={Stats} />
-    <Route path='/register' component={register} />
     <Route path='/characters/:id' component={Character} />
     <Route path='/add' component={AddCharacter} />
     <Route path=':category' component={CharacterList}>
