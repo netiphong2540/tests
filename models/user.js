d3b6182a0ab2e86e3869fdc1ed0f1e75336ebf4f
netiphong2540 <31153839+netiphong2540@users.noreply.github.com>
2017-12-15 21:04:43 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,3 +6,16 @@ var userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false, { message: 'Incorrect username.' });
+    user.comparePassword(password, function(err, isMatch) {
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+    });
+  });
+}));
